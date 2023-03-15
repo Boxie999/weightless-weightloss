7d727650f2173d71eafb5f3fa55182cd61289959
CcMong <fullstackcodex@gmail.com>
2023-03-15 17:51:34 +0000

    Downloaded React Slick to display search results in a carousel format. Then worked to get it working, along with navigation arrows on the side.

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -8,15 +8,15 @@ const ExerciseCard = ({ exercise }) => {
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
       <Stack direction="horizontal" gap={3}>
-        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+        <Button style={{color: "white", background: "orange", fontSize: "10px", textTransform: "capitalise"}}>
           {exercise.bodyPart}
 
         </Button>
-        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+        <Button style={{color: "white", background: "green", fontSize: "10px", textTransform: "capitalise"}}>
           {exercise.target}
 
         </Button>
-        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+        <Button style={{color: "white", background: "navy", fontSize: "10px", textTransform: "capitalise"}}>
           {exercise.equipment}
 
         </Button>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,36 +1,81 @@
 import React from 'react';
-import { Container, Row, Col, Stack } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 // import { searchOptions, retrieveApiData } from '../utils/retrieveApiData';
 import ExerciseCard from './ExerciseCard';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
+
+// First writing functions to create custom arrows for the React Slick carousel we will display the search results in.
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "silver" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "silver" }}
+        onClick={onClick}
+      />
+    );
+}  
+
+// Next, we will look to display the search results.
 // This will be the component within which the search results will be displayed on the screen. It will display the collection of results, which we store/track within the exerciseResults state
+
+// FIGURE OUT A WAY TO STOP IT REPEATING RESULTS WHEN THERE ARE FEWER THAN 4 RESULTS TO SHOW
+
 
 const SearchResults = ( { exerciseResults, setExerciseResults }) => {
     //console.log(exerciseResults);
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    
+    }
+
   return (
-    <Container id="searchResults" style={{marginTop: "40px", padding: "15px"}}>
+    
+    <Container fluid id="searchResults" style={{marginTop: "40px", padding: "15px"}}>
       <p>Showing Exercise Results</p>
       <br />
-      <Container>
+        <Container fluid>
 
-        <Stack direction="horizontal">
+        
+            <Slider {...settings}>
 
-            {exerciseResults.map((exercise, index) => (
+                {exerciseResults.map((exercise, index) => (
 
-            <Col>
+                    <Col>
 
-            <Row style={{whiteSpace: "nowrap", display: "inline-block"}}>
-                <ExerciseCard key={index} exercise={exercise} />
-            </Row>
-            
-            </Col>                                   
                     
-            ))}
-        </Stack>
+                        <ExerciseCard key={index} exercise={exercise} />
+                    
+                    
+                    </Col>                                   
+                        
+                ))}
+                
+            </Slider>      
 
-
-      </Container>
+        </Container>
       
     </Container>
   )
