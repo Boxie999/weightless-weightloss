dc99f2edafc26c0c4c72520d6b248986acca14d8
CcMong <fullstackcodex@gmail.com>
2023-03-15 15:19:07 +0000

    Created a new component, ExerciseCard, and wrote it into SearchResults which, in turn, will be placed within BuildWorkout. Props were then passed from BuildWorkout to SearchResults, and then from SearchResults to ExerciseCard.

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Container, Pagination, Stack, Row, Col } from "react-bootstrap";
+import ExerciseCard from './ExerciseCard';
 
-const SearchResults = () => {
+// This will be the component within which the search results will be displayed on the screen. It will display the collection of results, which we store/track within the exerciseResults state
+
+const SearchResults = ( { exerciseResults, setExerciseResults }) => {
+    console.log(exerciseResults);
+
   return (
-    <div>SearchResults</div>
+    <Container id="searchResults" style={{marginTop: "40px", padding: "15px"}}>
+      <p>Showing Exercise Results</p>
+      <br />
+      <Row>
+        
+        {exerciseResults.map((exercise, index) => (
+          <Col>          
+            <ExerciseCard key={index} exercise={exercise}/>            
+          </Col>
+          
+        ))}
+      
+      </Row>
+    </Container>
   )
 }
 
