6147167ff4f41303b79096ff20d7014ec6f613ed
CcMong <fullstackcodex@gmail.com>
2023-03-15 16:11:04 +0000

    Been able to adjust the SearchResults such that all the results display horizontally. Next will be trying to place them in a horizontal scrollbar.

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Button, Stack } from "react-bootstrap";
 
-const ExerciseCard = () => {
+
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="horizontal" gap={3}>
+        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+          {exercise.bodyPart}
+
+        </Button>
+        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+          {exercise.target}
+
+        </Button>
+        <Button style={{color: "cyan", background: "silver", fontSize: "15px", textTransform: "capitalise"}}>
+          {exercise.equipment}
+
+        </Button>
+      </Stack>
+      <p style={{fontWeight: "bold", color: "black", textDecoration: "none", textTransform: "capitalize" }}>{exercise.name}</p>
+
+    </Link>
   )
 }
 
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { Container, Pagination, Stack, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Stack } from "react-bootstrap";
+// import { searchOptions, retrieveApiData } from '../utils/retrieveApiData';
 import ExerciseCard from './ExerciseCard';
 
 // This will be the component within which the search results will be displayed on the screen. It will display the collection of results, which we store/track within the exerciseResults state
 
 const SearchResults = ( { exerciseResults, setExerciseResults }) => {
-    console.log(exerciseResults);
+    //console.log(exerciseResults);
 
   return (
     <Container id="searchResults" style={{marginTop: "40px", padding: "15px"}}>
       <p>Showing Exercise Results</p>
       <br />
-      <Row>
-        
-        {exerciseResults.map((exercise, index) => (
-          <Col>          
-            <ExerciseCard key={index} exercise={exercise}/>            
-          </Col>
-          
-        ))}
+      <Container>
+
+        <Stack direction="horizontal">
+
+            {exerciseResults.map((exercise, index) => (
+
+            <Col>
+
+            <Row style={{whiteSpace: "nowrap", display: "inline-block"}}>
+                <ExerciseCard key={index} exercise={exercise} />
+            </Row>
+            
+            </Col>                                   
+                    
+            ))}
+        </Stack>
+
+
+      </Container>
       
-      </Row>
     </Container>
   )
 }
