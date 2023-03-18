3861ef37f57cfbcbb9bc7f7d610a1ce76aecc9ae
DRRobertson9673 <drrobertson9673@gmail.com>
2023-03-18 02:08:30 +0000

    added array objects to workout picker that match those in list.js

--- src/pages/WorkoutPicker.js
+++ src/pages/WorkoutPicker.js
@@ -7,10 +7,42 @@ import upperBody from "../images/body/UpperBody.svg"
 import core from "../images/body/Core.svg"
 import legs from "../images/body/Legs.svg"
 
-const fullBodyPlan = [1, 2, 3];
-const upperBodyPlan = [4, 5, 6];
-const corePlan = [7, 8, 9];
-const legsPlan = [10, 11, 12];
+const fullBodyPlan =
+    [
+        { name: 'full body 1' },
+        { name: 'full body 2' },
+        { name: 'full body 3' },
+        { name: 'full body 4' },
+        { name: 'full body 5' },
+        { name: 'full body 6' }
+    ];
+const upperBodyPlan =
+[
+    { name: 'upper body 1' },
+    { name: 'upper body 2' },
+    { name: 'upper body 3' },
+    { name: 'upper body 4' },
+    { name: 'upper body 5' },
+    { name: 'upper body 6' }
+];
+const corePlan =
+[
+    { name: 'core 1' },
+    { name: 'core 2' },
+    { name: 'core 3' },
+    { name: 'core 4' },
+    { name: 'core 5' },
+    { name: 'core 6' }
+];
+const legsPlan =
+[
+    { name: 'legs 1' },
+    { name: 'legs 2' },
+    { name: 'legs 3' },
+    { name: 'legs 4' },
+    { name: 'legs 5' },
+    { name: 'legs 6' }
+];
 
 function WorkoutPicker() {
 
@@ -19,7 +51,7 @@ function WorkoutPicker() {
     const [CoreIsShown, setCoreIsShown] = useState(false);
     const [LegsIsShown, setLegsIsShown] = useState(false);
 
-    useEffect(() => { localStorage.setItem('dataKey', JSON.stringify(bodyPart)) }, [bodyPart]);
+    useEffect(() => { localStorage.setItem('workout plan', JSON.stringify(bodyPart)) }, [bodyPart]);
     useEffect(() => { document.body.style.backgroundColor = 'white' }, [])
     useEffect(() => { document.body.style.backgroundImage = 'linear-gradient(0deg, rgba(0, 233, 255, 0) 23%, rgba(62, 240, 220, 0) 80%)' }, [])
 
