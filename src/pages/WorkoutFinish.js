cea84a97e4de9d37d1de8c487c2ec8afb87c2ada
admin <admin@admins-MacBook-Pro.local>
2023-03-22 19:13:50 +0000

    added button on finish page and starter card next to list

--- src/pages/WorkoutFinish.js
+++ src/pages/WorkoutFinish.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import "../styles/WorkoutFinish.css"
 import finishImage from '../images/finishImage.png';
+import { Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 
 const WorkoutFinish = () => {
   return ( <div className='workoutFinish'>
     <div><p className='finish-text'>Yo! Do you feel like Rocky yet? 'Cause...</p></div>
     <div><h1 className='finish-heading'>YOU ROCKED YOUR EXERCISE!</h1></div>
     <div><img className='finishImage' src={finishImage} alt='Finish workout image' /></div>
+    <div><NavLink to="/">
+                        <button className="btn btn-primary border-0 btn-lg chooseButton" href="#" type="button">GO TO HOMEPAGE</button>
+                    </NavLink></div>
     </div>
   )
 }
